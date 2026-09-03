d8b40dbdc15efe174ba043ddd9099ae4f1165d11
DariuszLasota <dariusz.lasota@innowise.com>
2026-09-03 14:27:09 +0200

    comment entity imports

--- apps/core_microservice/src/shared/data-source.ts
+++ apps/core_microservice/src/shared/data-source.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
-import { AccountOrmEntity } from './db-entities/account.orm-entity.js';
-import { AssetOrmEntity } from './db-entities/asset.orm-entity.js';
-import { ChatOrmEntity } from './db-entities/chat.orm-entity.js';
-import { CommentOrmEntity } from './db-entities/comment.orm-entity.js';
-import { MessageOrmEntity } from './db-entities/message.orm-entity.js';
-import { PostOrmEntity } from './db-entities/post.orm-entity.js';
-import { ProfileConfigOrmEntity } from './db-entities/profile-config.orm-entity.js';
-import { ProfileOrmEntity } from './db-entities/profile.orm-entity.js';
-import { UserOrmEntity } from './db-entities/user.orm-entity.js';
+// import { AccountOrmEntity } from './db-entities/account.orm-entity.js';
+// import { AssetOrmEntity } from './db-entities/asset.orm-entity.js';
+// import { ChatOrmEntity } from './db-entities/chat.orm-entity.js';
+// import { CommentOrmEntity } from './db-entities/comment.orm-entity.js';
+// import { MessageOrmEntity } from './db-entities/message.orm-entity.js';
+// import { PostOrmEntity } from './db-entities/post.orm-entity.js';
+// import { ProfileConfigOrmEntity } from './db-entities/profile-config.orm-entity.js';
+// import { ProfileOrmEntity } from './db-entities/profile.orm-entity.js';
+// import { UserOrmEntity } from './db-entities/user.orm-entity.js';
 
 export const dataSource = new DataSource({
   type: 'postgres',
@@ -19,15 +19,15 @@ export const dataSource = new DataSource({
   database: process.env.POSTGRES_DB,
   synchronize: false,
   entities: [
-    AccountOrmEntity,
-    AssetOrmEntity,
-    ChatOrmEntity,
-    CommentOrmEntity,
-    MessageOrmEntity,
-    PostOrmEntity,
-    ProfileConfigOrmEntity,
-    ProfileOrmEntity,
-    UserOrmEntity
+    // AccountOrmEntity,
+    // AssetOrmEntity,
+    // ChatOrmEntity,
+    // CommentOrmEntity,
+    // MessageOrmEntity,
+    // PostOrmEntity,
+    // ProfileConfigOrmEntity,
+    // ProfileOrmEntity,
+    // UserOrmEntity
   ],
   migrations: ['./src/shared/migrations/*{.ts,.js}']
 });
